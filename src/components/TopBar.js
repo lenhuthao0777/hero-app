0911471c56689dfbd867b6a7de7313fc71b9c679
Le Nhut Hao <lenhuthao0777@gmail.com>
2021-09-14 16:46:41 +0700

    feat(topbar): top bar done

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TopBar = () => {
+  const [btnActive, setBtnActive] = useState(0)
     return (
-        <div className="container">
+        <div className="top-bar">
+            <div className="container">
+                <h1>Tour of Heroes</h1>
+                <button className={`btn ${btnActive === 1 ? "btn--active" : ""}`} onClick={()=>setBtnActive(1)}>Dashboard</button>
+                <button className={`btn ${btnActive === 2 ? "btn--active" : ""}`} onClick={()=>setBtnActive(2)}>Heroes</button>
+            </div>
         </div>
     );
 };
