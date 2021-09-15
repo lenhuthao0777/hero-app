72731abbf81be8c0fe32ca1050784638ad39c15c
Le Nhut Hao <lenhuthao0777@gmail.com>
2021-09-15 09:32:22 +0700

    feat(dashboard): dashboard done

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
+    const { data } = useSelector((state) => state.DashboardReducer);
     return (
         <div className="dashboard">
-           
+            <div className="container">
+                <h2>Top Heroes</h2>
+                <div className="dashboard__list">
+                    {data.slice(1, 5).map((item) => (
+                        <Link key={item.id} to="/detail">{item.name}</Link>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
